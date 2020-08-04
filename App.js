841bedb78917cff80d6b3ced5912a9a237bf00fd
abusamrah2005 <abusamrah2005@gmail.com>
2020-08-05 01:39:16 +0300

    ss

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
 import { Button, colors } from 'react-native-elements';
 export default function App() {
   return (
@@ -11,13 +10,6 @@ export default function App() {
       <Text>Open up App.jdfdgfg</Text>
       <Button title='Add New Student' icon={{ name: 'code' }} />
 
-=======
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
->>>>>>> 4c42ae231cc1e909d23ac349668d564309506d22
       <StatusBar style='auto' />
     </View>
   );
@@ -26,11 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
-    backgroundColor: '#000000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    image: './',
   },
   text: {
     backgroundColor: 'blue',
@@ -40,10 +30,5 @@ const styles = StyleSheet.create({
   },
   Button: {
     backgroundColor: '#000',
-=======
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
->>>>>>> 4c42ae231cc1e909d23ac349668d564309506d22
   },
 });
